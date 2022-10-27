145eb57dd789eec6f846709a644c759f25144aa1
jijijiwonton <ji04wonton30@gmail.com>
2022-10-27 04:22:54 -0400

    Styling fix (1)

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 "use strict";
 
+// Define a global variable
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+const navbarChangeMode = "navbar__dark";
+
+/**
+ * A method to change navbar background when the scrolling position is higher than navbar height
+ */
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add(navbarChangeMode);
+  } else {
+    navbar.classList.remove(navbarChangeMode);
+  }
+});
+
 /**
  * Handling a scroll action when clicking on the navbar menu
  */
